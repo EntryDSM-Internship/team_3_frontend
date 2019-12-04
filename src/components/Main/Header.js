c11fe97338c3a601ec1410bc7189628d9823dbd0
woochanlee <030219woo@naver.com>
2019-12-04 20:22:27 +0900

    [ADD] Header
    finish style

--- src/components/Main/Header.js
+++ src/components/Main/Header.js
@@ -3,14 +3,66 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 
+const Wrapper = styled.div`
+    border-bottom: 2px solid #505BDA;  
+    padding-left: 12px;
+    box-sizing: border-box;
+    main {
+        width: auto;
+        max-width: 1140px;
+        margin: 0 auto;
+        height: 57px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        div {
+            flex: 1;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            img {
+                width: 28px;
+                height: 27px;
+            }
+        }
+        ul {
+            padding: 0;
+            margin: 0;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            li {    
+                width: 74px;
+                height: 57px;
+                list-style: none;
+                text-align: center;
+                line-height: 60px;
+                margin: 0;
+                box-sizing: border-box;
+                &.under_line {
+                    border-bottom: 4px solid #505BDA;
+                }
+                a {
+                    height: 18px;
+                    font-weight: bold;
+                    font-size: ;
+                    text-decoration: none;
+                    color: #000000; 
+                }
+            }
+        }
+    }   
+`;
 
 const Header = () => {
     return (
-        <div>
+        <Wrapper>
             <main>
-                <img src={require("../../img/Main/MainIcon.png")} alt="아이콘"/>
+                <div>
+                    <img src={require("../../img/Main/MainIcon.png")} alt="아이콘" />
+                </div>
                 <ul>
-                    <li>
+                    <li className="under_line">
                         <Link to="/main">홈</Link>
                     </li>
                     <li>
@@ -21,7 +73,7 @@ const Header = () => {
                     </li>
                 </ul>
             </main>
-        </div>
+        </Wrapper>
     );
 };
 
