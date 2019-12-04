cedfcf4230a4b0542b647b672e3cf17f73bb63a5
woochanlee <030219woo@naver.com>
2019-12-04 18:58:04 +0900

    [ADD] header
    making the jsx

--- src/components/Main/Header.js
+++ src/components/Main/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+
 
 const Header = () => {
     return (
         <div>
-            
+            <main>
+                <img src={require("../../img/Main/MainIcon.png")} alt="아이콘"/>
+                <ul>
+                    <li>
+                        <Link to="/main">홈</Link>
+                    </li>
+                    <li>
+                        <Link to="/search">검색</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">프로필</Link>
+                    </li>
+                </ul>
+            </main>
         </div>
     );
 };
