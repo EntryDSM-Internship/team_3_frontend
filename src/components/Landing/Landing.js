2e6f9cc4227b669902ca38296e6f2ca1bb374435
woochanlee <030219woo@naver.com>
2019-12-05 19:21:28 +0900

    [ADD] Landing jsx

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LandingWrapper from '../../styles/Landing';
+
+const Landing = () => {
+    return (
+        <LandingWrapper>
+            <div>
+                <main>
+                    <h1>지금 세계 곳곳에서 무슨 일이<br/>일어나고 있는지 확인하세요.</h1>                    
+                </main>
+            </div>
+        </LandingWrapper>
+    );
+};
+
+export default Landing;
---
+++ src/styles/Landing.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const LandingWrapper = styled.div`
+    background-color: #737be1;
+    margin: 0 auto;
+    div {
+        max-width: 1170px;
+        main {
+            width: 405px;
+            height: 548px;
+        }
+    }
+`;;
