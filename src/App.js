b8e31641230127396ba380d80bb8f1c4f35ba0c1
woochanlee <030219woo@naver.com>
2019-12-05 22:18:34 +0900

    [ADD] create Login files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import SignUp from './components/SignUp/SignUp';
 import Main from './components/Main/Main';
+import Login from './components/Login/Login';
+
 import { Route } from 'react-router-dom';
 
 const App = () => {
@@ -8,6 +10,7 @@ const App = () => {
     <>
       <Route exact path="/:category?" component={Main} />
       <Route exact path="/signup" component={SignUp}/>
+      <Route exact path="/login" component={Login} />
     </>
   );
 };
---
+++ src/components/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { LoginWrapper } from './styles';
+const Login = () => {
+    return (
+        <LoginWrapper>
+            
+        </LoginWrapper>
+    );
+};
+
+export default Login;
