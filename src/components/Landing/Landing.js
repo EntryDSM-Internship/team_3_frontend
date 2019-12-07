a09ffc873f138e0b58790c39816a2fe783ffdfde
woochanlee <030219woo@naver.com>
2019-12-07 22:12:25 +0900

    [UPDATE] separate div to styled-component

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { LandingWrapper } from '../../styles/Landing';
+import { LandingWrapper, MainBlock, Main, GuideContent } from './styles';
 import messeage from '../../img/Landing/messeage.png';
 import people from '../../img/Landing/people.png';
 import search from '../../img/Landing/search.png';
@@ -8,25 +8,25 @@ import search from '../../img/Landing/search.png';
 const Landing = () => {
     return (
         <LandingWrapper>
-            <div>
-                <main>
+            <MainBlock>
+                <Main>
                     <h1>지금 세계 곳곳에서 무슨 일이<br/>일어나고 있는지 확인하세요.</h1>                    
-                    <div>
+                    <GuideContent>
                         <img src={search} alt="검색" />
                         <p>다른 사람을 찾아보세요.</p>
-                    </div>
-                    <div>
+                    </GuideContent>
+                    <GuideContent>
                         <img src={people} alt="사람" />
                         <p>자신의 일을 사람들과 함께 나눠보세요.</p>
-                    </div>
-                    <div>
+                    </GuideContent>
+                    <GuideContent>
                         <img src={messeage} alt="메시지" />
                         <p>다른 사람의 일에 자신의 의견을 올려보세요.</p>
-                    </div>
+                    </GuideContent>
                     <Link to="/login" className="login">로그인</Link>
                     <Link to="/signup" className="sign-up">가입하기</Link>
-                </main>
-            </div>
+                </Main>
+            </MainBlock>
         </LandingWrapper>
     );
 };
