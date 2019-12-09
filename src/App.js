e5d7b380afe56f058a8496584cce44aecd4174bb
woochanlee <030219woo@naver.com>
2019-12-09 19:23:49 +0900

    [UPDATE] route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SignUp from './components/SignUp/SignUp';
 import Main from './components/Main/Main';
 import Landing from './components/Landing/Landing';
 import Login from './components/Login/Login';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
   const [isLogin, setIsLogin] = useState({
@@ -12,11 +12,17 @@ const App = () => {
   });
   return (
     <>
-        <Route exact path="/main/:category?" render={() => <Main isLogin={isLogin} setIsLogin={setIsLogin} />}/>
-           <Route exact path="/auth/login" render={() => <Login isLogin={isLogin} setIsLogin={setIsLogin} />} />
-            <Route exact path="/auth/signup" component={SignUp} />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/landing" render={() => <Landing isLogin={isLogin} setIsLogin={setIsLogin} />}/>
+      <Route exact path="/auth/login" render={() => <Login isLogin={isLogin} setIsLogin={setIsLogin} />} />
+      <Route exact path="/auth/signup" component={SignUp} />
+      <Route exact path="/landing" render={() => <Landing isLogin={isLogin} setIsLogin={setIsLogin} />} />
+      <Switch>
+        {
+          (isLogin.access_token === "" && (<Route exact path="/" render={() => <Landing isLogin={isLogin} setIsLogin={setIsLogin} />} />)) ||
+          (<Route exact path={["/:category", "/"]} render={() => <Main isLogin={isLogin} setIsLogin={setIsLogin} />} />)
+        }  
+      </Switch>
+    
+      <Route render={() => (<h1>Error</h1>)} />
     </>
   );
 };
