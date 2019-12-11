4f2c50405ddfe88673238a02b7cd80f28a444fc2
woochanlee <030219woo@naver.com>
2019-12-11 21:33:31 +0900

    [UPDATE] change styled-components

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import * as S from './styles';
-import BoardItem from './BoardItem';
-import ViewMore from './ViewMore';
+import BoardList from "./BoardList";
+
+
 
 const Home = () => {
     return (
-        <S.HomeWrapper>
-            <S.MainWrapper>
-                <S.BoardWrapper>
-                    <BoardItem />
-                </S.BoardWrapper>
-                <S.SideWrapper>
-                    <ViewMore />
-                </S.SideWrapper>
-            </S.MainWrapper>
-        </S.HomeWrapper>
+      <S.BoardWrapper>
+        <BoardList />
+      </S.BoardWrapper>
     );
 };
 
--- src/components/Main/Home/styles.js
+++ src/components/Main/Home/styles.js
@@ -2,21 +2,6 @@ import styled from 'styled-components';
 import Like from '../../../img/Main/Like.png';
 import Comment from '../../../img/Main/Comment.png';
 
-
-export const HomeWrapper = styled.div`
-    background-color: #fbfbfb;
-    height: 887px;
-`;
-
-export const MainWrapper = styled.div`
-    display: flex;
-    width: auto;
-    height: 100%;
-    max-width: 1140px;
-    margin: 0 auto;
-    background-color: #ffffff;
-`;
-
 export const BoardWrapper = styled.div`
     overflow-y: scroll;
     -ms-overflow-style: none; 
@@ -29,33 +14,42 @@ export const BoardWrapper = styled.div`
     }
 `;
 
-export const SideWrapper = styled.div`
-    width: 422px;
-`;
+
 
 export const BoardItemWrapper = styled.div`
-    display: flex;
-    padding: 15px;
-    box-sizing: border-box;
-    width: 100%;
-    min-height: 148px;
-    height: auto;
-    border-bottom: 3px solid rgba(249, 250, 254, 1);
-`;
+         display: flex;
+         padding: 15px;
+         box-sizing: border-box;
+         width: 100%;
+         /* min-height: 148px; */
+         height: auto;
+         border-bottom: 3px solid rgba(249, 250, 254, 1);
+         box-sizing:border-box;
+         /* & + & {
+           border-bottom: 3px solid rgba(249, 250, 254, 1);
+         }
+         &:nth-child(last) {
+           border-bottom: 3px solid rgba(249, 250, 254, 1);
+         } */
+       `;
 
 export const ProfileBlock = styled.div`
     width: 49px;
-    min-height: 115px;
+    /* min-height: 115px; */
+    min-height: 49px;
+    height: auto;
     margin: 0;
     img {
         width: 49px;
         height: 49px;
+        cursor: pointer;
     }
 `;
 
 export const ShareBoardWrapper = styled.div`
     width: 856px;
-    min-height: 115px;
+    /* min-height: 115px; */
+    height: auto;
     margin-left: 15px;
     margin-top: 14px;
     box-sizing: border-box;
@@ -66,7 +60,7 @@ export const ShareBoardWrapper = styled.div`
         overflow: scroll;
         resize: none;
         max-width: 593px;
-        min-height: 65px;
+        min-height: 65px;   
         height: auto;
         border: none;
         font-size: 18px;
@@ -110,7 +104,8 @@ export const ShareBoardWrapper = styled.div`
 
 export const ContentsWrapper = styled.div`
     width: 856px;
-    min-height: 115px;
+    /* min-height: 115px; */
+    height: auto;
     margin-left: 15px;
     box-sizing: border-box;
     display: flex;
@@ -168,55 +163,3 @@ export const ContentsWrapper = styled.div`
     }
 `;
 
-export const ViewMoreBlock = styled.div`
-    width: 360px;
-    height: 405px;
-    margin: 60px 15px 0 45px;
-    padding: 15px 0;
-    border-radius: 16px;
-    background-color: #dbdeff;
-    header { 
-        padding: 0 15px;
-        height: 37px;
-        border-bottom: 2px solid rgba(214, 230, 224, 1);
-        box-sizing: border-box;
-        h1 {
-            margin: 0;
-            font-size: 20px;
-            font-weight: bold;
-        }
-    }
-    ul {
-        padding: 15px;
-        margin: 0;
-        li {
-            height: 50px;
-            list-style: none;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            h2 {
-                margin: 0;
-                width: 79px;
-                height: 18px;
-                font-size: 14px;
-                line-height: 1.13;
-                letter-spacing: 0;
-                text-align: left;
-                color: #3b3b3b;
-            }
-            img {
-                cursor: pointer;
-                width: 20px;
-                height: 20px;
-                background-size: contain;
-            }
-        }
-        li ~ li {
-            border-top: 2px solid rgba(214, 230, 224, 1);
-        }
-        div {   
-            display: none;
-        }
-    }
-`;
