be5f7c72a0b1f54e075c0a020924ec27dec078c7
woochanlee <030219woo@naver.com>
2020-01-10 15:17:47 +0900

    [MODIFY] <ViewMoreBlock> styled change

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -45,9 +45,9 @@ export const ViewMoreBlock = styled.div`
             align-items: center;
             justify-content: space-between;
             border-top: 2px solid rgba(214, 230, 224, 1);
-            h2 {
+            > h2 {
                 margin: 0;
-                width: 84px;
+                width: auto;
                 height: 18px;
                 font-size: 14.9799px;
                 line-height: 18px;
@@ -62,9 +62,56 @@ export const ViewMoreBlock = styled.div`
                 background-size: contain;
             }
         }
-        div {   
+        > div {   
             display: none;
             padding: 16px 15px;
+            > h2 {
+                margin: 0;
+                width: auto;
+                height: 18px;
+                font-size: 14.9799px;
+                line-height: 18px;
+                letter-spacing: 0;
+                text-align: left;
+                color: #3b3b3b;
+                margin-bottom: 15px;
+                display: inline-block;
+            }
+            #switch_background {
+                padding: 2px 4px;
+                float: right;
+                display: flex;
+                margin-top: 4px;
+                /* justify-content: flex-end; */
+                align-items: center;
+                width: 28px;
+                height: 12px;
+                border-radius: 12px;
+                background-color: #000000;
+                /* &.on {
+                    animation: switchOn .3s;
+                    justify-content: flex-end;
+                }
+                &.off {
+                    animation: switchOff .3s;
+                    justify-content: flex-start;
+                } */
+                > #switch_circle {
+                    float: right;
+                    width: 10px;
+                    height: 10px;
+                    border-radius: 50%;
+                    background-color: #dbdeff;
+                }
+                @keyframes switchOn {
+                from { justify-content: flex-start; }
+                to { justify-content: flex-end; }
+                }
+                @keyframes switchOff {
+                from { justify-content: flex-end; }
+                to { justify-content: flex-start; }
+                }
+            }
         }
     }
 `;
