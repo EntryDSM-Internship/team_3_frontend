843048e9aea4492cfc77fd322809bf576dda9be4
woochanlee <030219woo@naver.com>
2020-01-10 14:54:37 +0900

    [DELETE] delete comments

--- src/App.js
+++ src/App.js
@@ -6,26 +6,19 @@ import Login from './components/Login/Login';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const App = () => {
-  const [isLogin, setIsLogin] = useState({
-    "access_token": "",
-    "refresh_token": ""
+  const [token, setToken] = useState({
+    "access_token": localStorage.getItem("access_token"),
+    "refresh_token": localStorage.getItem("refresh_token")
   });
-  useState(() => {
-    setIsLogin({
-      "access_token": localStorage.getItem("access_token"),
-      "refresh_token": localStorage.getItem("refresh_token")
-    });
-  }, []);
   
-
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/auth/login" render={() => <Login isLogin={isLogin} setIsLogin={setIsLogin} />} />
+        <Route exact path="/auth/login" render={() => <Login token={token} setToken={setToken} />} />
         <Route exact path="/auth/signup" component={SignUp} />
-        <Route exact path="/landing" render={() => <Landing isLogin={isLogin} setIsLogin={setIsLogin} />} />
-        <Route exact path={"/:category"} render={({ location }) => (<Main isLogin={isLogin} setIsLogin={setIsLogin} component={Main} location={location} />)} />
-        <Route exact path="/" render={() => <><Main isLogin={isLogin} setIsLogin={setIsLogin} /><Landing isLogin={isLogin} setIsLogin={setIsLogin} /></>} />
+        <Route exact path="/landing" render={() => <Landing category="/landing" token={token} setToken={setToken} />} />
+        <Route path={"/:category"} render={({ match, location }) => (<Main alertOn={true} token={token} setToken={setToken} component={Main} location={location} match={match} />)}/>
+        <Route exact path="/" render={({ location }) => <><Main alertOn={false} location={location} token={token} setToken={setToken} /><Landing alertOn={false} category="/" token={token} setToken={setToken} /></>} />
         <Route render={() => (<h1>Error</h1>)} />
       </Switch>
     </BrowserRouter>
